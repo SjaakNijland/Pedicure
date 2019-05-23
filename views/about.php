c61c955f11a57a77f77a55af7ed6d20f64a27def
SjaakNijland <wafelcool@gmail.com>
2019-05-23 13:17:03 +0200

    over ons en behandeling pagina

---
+++ views/about.php
@@ -0,0 +1,60 @@
+<div class="container">
+    <div class="page-banner">
+        <img src="img/banner.png">
+        <div class="background-layer"></div>
+    </div>
+    <div class="inner">
+        <div class="page-info">
+            <p class="title">Over mij</p>
+            <p class="quote">"Uw voeten in goeden handen, Daar maak ik graag werk van!"</p>
+            <div class="about-image">
+                <p class="about-blue-title mobile">Mijn naam is <b>Joke Sol</b>. Ik ben gediplomeerd medisch pedicure.</p>
+                <div class="layer-1">
+                    <img src="img/shape-blue.png" alt="">
+                    <img class="layer-2" src="img/about.jpg" alt="">
+                </div>
+            </div>
+            <div class="about-text">
+                <p class="about-blue-title desktop">Mijn naam is <b>Joke Sol</b>. Ik ben gediplomeerd medisch pedicure.</p>
+                <p class="about-title">Loopbaanswitch</p>
+                <p>
+                  Na een  carrière als loopbaanadviseur ontstond bij mijzelf de behoefte aan verandering.
+                  Een praktisch beroep waarbij ik mensen blij kon maken. Zo kwam het pedicure-werk voor mij in beeld: mensen helpen door de druk of pijn die ze aan hun voeten hebben voor een groot deel weg te nemen.
+                </p>
+                <p>
+                  Tijdens mijn opleiding merkte ik hoe fijn het is om mensen weer op weg te helpen naar gezonde, verzorgde en minder pijnlijke voeten. De keuze om na de branche-opleiding pedicure door te leren voor Medisch Pedicure was dan ook vanzelfsprekend voor mij.
+                  Ik hoop nog heel lang mensen te kunnen helpen met goede voetzorg en nieuwe ontwikkelingen in het vakgebied te kunnen volgen en gebruiken voor mijn klanten.
+                </p>
+          </div>
+        </div>
+    </div>
+    <div class="partners">
+        <div class="inner">
+            <p class="title">Partners</p>
+        </div>
+        <div class="partners-logo">
+            <div class="inner">
+                <div class="logos">
+                    <img src="img/provoet.png" alt="">
+                    <img src="img/hyp.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="more-info">
+        <div class="inner">
+            <div class="info-block blue">
+                <p>Wilt u meer weten over de <b>behandeling</b> die ik aanbied</p>
+                <a href="" class="button"> Bekijk Behandelingen</a>
+            </div>
+            <div class="info-block orange">
+                <p>Wilt u <b>tips</b> over hoe u zelf voeten kunt vezorgen?</p>
+                <a href="" class="button"> Bekijk Tips</a>
+            </div>
+            <div class="info-block blue">
+                <p>Benieuwd naar de <b>Tarieven</b> van u behandeling?</p>
+                <a href="" class="button"> Bekijk Tarieven</a>
+            </div>
+        </div>
+    </div>
+</div>
